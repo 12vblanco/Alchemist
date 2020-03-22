1bf181174c96910054326201a3b712eda90076d4
Rigoz13 <52953604+Rigoz13@users.noreply.github.com>
2020-03-22 10:03:04 +0000

    contact

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST{'submit'})) {
+  $name = $_POST{'name'};
+  $email = $_POST{'email'};
+  $phone = $_POST{'phone'};
+  $message = $_POST{'message'};
+
+  $mailto = "vblanco@mail.com";
+  $headers = "From: ".$email, $phone;
+  $txt = "You have received an email form ".$name." .\n\n".$message;
+
+  email($mailto, $phone, $txt, $headers);
+  header("Location: index.php?mailsend");
+}
