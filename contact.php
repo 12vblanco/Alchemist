866fc04213ed3475aa9ad4b65d981d66d1481fb8
Rigoz13 <52953604+Rigoz13@users.noreply.github.com>
2020-03-23 15:37:32 +0000

    php

--- contact.php
+++ contact.php
@@ -1,35 +1,15 @@
 <?php
- $name = $_POST['name'];
- $visitor_email = $_POST['email'];
- $message = $_POST['message'];
-
-$email_from = 'www.thealchemistscavetattoo.com';
-$email_subject = "New From Submission";
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                  "User Message: $message.\n";
-$to = "vblanco@mail.com";
-
-$headers = "From: $email_from \r\n";
-
-$headers = "Reply-to: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-header("Location: index.html");
-
-?>
-
-<!--  
-//  if (isset($_POST['submit']))
-//    $name = $_POST['name'];
-//    $email = $_POST['email'];
-//    $phone = $_POST['phone'];
-//    $message = $_POST['message'];
-
-//    $mailto = "vblanco@mail.com";
-//    $headers = "From: ".$email;
-//    $txt = "You have received an email form ".$name." .\n\n".$message" .\n\n".$phone";
-
-//    email($mailto, $txt, $headers);
-//    header("Location: index.php?mailsend");
-// ?>-->
+  if (isset($_POST['email']))  {
+  
+    //Email information
+    $admin_email = "vblanco@mail.com";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    
+    //send email
+    mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
+    
+    header('Location: http://edgeledger.net/success.html');
+  }
