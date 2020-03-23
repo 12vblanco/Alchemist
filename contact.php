5e3628b5aced38b66918fca67941d6b85bd3040f
Rigoz13 <52953604+Rigoz13@users.noreply.github.com>
2020-03-23 08:54:29 +0000

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,15 +1,20 @@
 <?php
+ $name = $_POST[]
 
-if (isset($_POST{'submit'})) {
-  $name = $_POST{'name'};
-  $email = $_POST{'email'};
-  $phone = $_POST{'phone'};
-  $message = $_POST{'message'};
 
-  $mailto = "vblanco@mail.com";
-  $headers = "From: ".$email;
-  $txt = "You have received an email form ".$name." .\n\n".$message;
 
-  email($mailto, $txt, $headers);
-  header("Location: index.php?mailsend");
+
+
+ if (isset($_POST['submit']))
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $phone = $_POST['phone'];
+   $message = $_POST['message'];
+
+   $mailto = "vblanco@mail.com";
+   $headers = "From: ".$email;
+   $txt = "You have received an email form ".$name." .\n\n".$message;
+
+   email($mailto, $txt, $headers);
+   header("Location: index.php?mailsend");
 ?>
